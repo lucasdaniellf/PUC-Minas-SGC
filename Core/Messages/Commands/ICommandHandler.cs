4aeb4328af0e9a2545d540d881642a3d315550d8
Lucas Figueiredo <lucas.daniel.lf@gmail.com>
2023-04-15 14:06:57 -0300

    untrack files with .gitignore

--- Core/Messages/Commands/ICommandHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Core.Messages.Commands
-{
-    public interface ICommandHandler<in TRequest, TResponse> where TRequest : CommandRequest
-    {
-        public Task<TResponse> Handle(TRequest command, CancellationToken token);
-    }
-}
--- Core/Messages/Event/IEventHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Core.Messages.Event
-{
-    public interface IEventHandler<in Request> where Request : EventRequest
-    {
-        public Task Handle(Request request, CancellationToken token);
-    }
-}
